e46a17334e15354803fd3ab25b4122ae3f1a8c4f
lomsadze_gi <lomsadze_gi@open.ru>
2017-01-30 15:57:07 +0300

    StoryBook for Divider

---
+++ src/Divider/Divider.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { muiTheme } from 'storybook-addon-material-ui';
+import { withKnobs, text, boolean, array, number, select } from '@kadira/storybook-addon-knobs';
+
+import Divider from './Divider';
+import Paper from '../Paper';
+import TextField from '../TextField';
+
+const style = {
+  marginLeft: 20,
+};
+
+storiesOf('Divider', module)
+  .addDecorator(withKnobs)
+  .addDecorator(muiTheme())
+  .add(null, () => (
+    <div style={{'padding': text('Padding', '50px')}}>
+        <Paper zDepth={2}>
+            <TextField hintText='First name' style={style} underlineShow={false} />
+            <Divider inset={boolean('Divider1 Inset', false)} />
+            <TextField hintText='Middle name' style={style} underlineShow={false} />
+            <Divider inset={boolean('Divider2 Inset', false)} />
+            <TextField hintText='Last name' style={style} underlineShow={false} />
+            <Divider inset={boolean('Divider3 Inset', false)} />
+            <TextField hintText='Email address' style={style} underlineShow={false} />
+            <Divider inset={boolean('Divider4 Inset', false)} />
+        </Paper>
+    </div>
+  ));
