108d71787371dfa475878d5b765fbe4791b56ff9
lomsadze_gi <lomsadze_gi@open.ru>
2017-01-31 12:24:58 +0300

    StoryBook for LinearProgress

---
+++ src/LinearProgress/LinearProgress.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { muiTheme } from 'storybook-addon-material-ui';
+import { withKnobs, text, boolean, array, number, select } from '@kadira/storybook-addon-knobs';
+
+import LinearProgress from './LinearProgress';
+
+storiesOf('LinearProgress', module)
+  .addDecorator(withKnobs)
+  .addDecorator(muiTheme())
+  .add(null, () => (
+    <div style={{'padding': text('Padding', '50px')}}>
+        <LinearProgress
+            color={text('Color', null)}
+            min={number('Min', 0)}
+            max={number('Max', 100)}
+            value={number('Value', 0)}
+            size={number('Size', 40)}
+            mode={select('Mode', ['determinate', 'indeterminate'], 'indeterminate')}
+            style={{width: number('Width', null)}}
+        />
+    </div>
+  ));
