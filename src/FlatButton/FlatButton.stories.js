99a5ff4f0daacc9f3eb397a4d519b95c85319e7e
lomsadze_gi <lomsadze_gi@open.ru>
2017-01-31 09:21:11 +0300

    StoryBook for FlatButton

---
+++ src/FlatButton/FlatButton.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { muiTheme } from 'storybook-addon-material-ui';
+import { withKnobs, text, boolean, array, number, select } from '@kadira/storybook-addon-knobs';
+
+import FlatButton from './FlatButton';
+import ActionAndroid from '../svg-icons/action/android';
+
+storiesOf('FlatButton', module)
+  .addDecorator(withKnobs)
+  .addDecorator(muiTheme())
+  .add(null, () => (
+    <div style={{'padding': text('Padding', '50px')}}>
+        <FlatButton
+            label={text('Label', 'Label')}
+            primary={boolean('Primary', true)}
+            secondary={boolean('Secondary', false)}
+            disabled={boolean('Disabled', false)}
+            icon={boolean('Icon', true) && <ActionAndroid />}
+            labelPosition={select('Label Position', ['after', 'before'], 'after')}
+            backgroundColor={text('Background Color', '')}
+            hoverColor={text('Hover Color', '')}
+            rippleColor={text('Ripple Color', '')}
+            href={text('Href', '')}
+            target={text('Target', '_blank')}
+            onKeyboardFocus={action('KeyboardFocus')}
+        />
+    </div>
+  ));
