e61da84eafdf1e3c9d9f41388eda6c362ec360b2
lomsadze_gi <lomsadze_gi@open.ru>
2017-02-01 15:24:33 +0300

    CheckBox story examples from material-ui docs

--- src/Checkbox/CheckBox.stories.js
+++ src/Checkbox/CheckBox.stories.js
@@ -5,18 +5,53 @@ import { muiTheme } from 'storybook-addon-material-ui';
 import { withKnobs, text, boolean, array, number, select } from '@kadira/storybook-addon-knobs';
 
 import Checkbox from './Checkbox';
+import ActionFavorite from '../svg-icons/action/favorite';
+import ActionFavoriteBorder from '../svg-icons/action/favorite-border';
+import Visibility from '../svg-icons/action/visibility';
+import VisibilityOff from '../svg-icons/action/visibility-off';
+
+const styles = {
+  checkbox: {
+    marginBottom: 16,
+  },
+};
 
 storiesOf('Checkbox', module)
   .addDecorator(withKnobs)
   .addDecorator(muiTheme())
   .add(null, () => (
-    <div style={{'padding': text('Padding', '50px')}}>
-      <Checkbox
-        label={text('Label', 'Label')}
-        checked={boolean('Checked', false)}
-        disabled={boolean('Disabled', false)}
-        labelPosition={select('Label Position', ['left', 'right'], 'right')}
-        onCheck={action('Check')}
-      />
+    <div style={{padding: text('Padding', '50px'), maxWidth: 250}}>
+        <Checkbox
+          label="Simple"
+          style={styles.checkbox}
+        />
+        <Checkbox
+          checkedIcon={<ActionFavorite />}
+          uncheckedIcon={<ActionFavoriteBorder />}
+          label="Custom icon"
+          style={styles.checkbox}
+        />
+        <Checkbox
+          checkedIcon={<Visibility />}
+          uncheckedIcon={<VisibilityOff />}
+          label="Custom icon of different shapes"
+          style={styles.checkbox}
+        />
+        <Checkbox
+          label="Disabled unchecked"
+          disabled={true}
+          style={styles.checkbox}
+        />
+        <Checkbox
+          label="Disabled checked"
+          checked={true}
+          disabled={true}
+          style={styles.checkbox}
+        />
+        <Checkbox
+          label="Label on the left"
+          labelPosition="left"
+          style={styles.checkbox}
+        />
     </div>
   ));
